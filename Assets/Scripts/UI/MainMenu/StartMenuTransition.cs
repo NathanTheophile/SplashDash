80d8d5327f6d696b3038079c001c36e531c9370c
Raphael <guiberaphael@gmail.com>
2026-09-09 16:05:48 +0200

    Main Menu Anim

--- Assets/Scripts/UI/MainMenu/StartMenuTransition.cs
+++ Assets/Scripts/UI/MainMenu/StartMenuTransition.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class StartMenuTransition : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup _nextMenuCanvasGroup;
+    [SerializeField] private CanvasGroup _buttonsCanvasGroup;
     [SerializeField] private GameObject _nextMenu;
     [SerializeField] private GameObject _deactivated;
     [SerializeField] private AnyButtonInput _buttonInput;
@@ -15,10 +15,15 @@ public class StartMenuTransition : MonoBehaviour
     [SerializeField] private RectTransform _startRect;
     [SerializeField] private float _Duration = 0.5f;
 
-    [Header("Colors")]
+    [Header("Fishes")]
     [SerializeField] private Image _fishes;
     [SerializeField] private Color _startColor;
     [SerializeField] private Color _endColor;
+
+    [Header("Bouee")]
+    [SerializeField] private TweenUIBetweenTwoPoints _tweenersBouee1;
+    [SerializeField] private TweenUIBetweenTwoPoints _tweenersBouee2;
+    [SerializeField] private CanvasGroup _boueeShadows;
 
     private void Start()
     {
@@ -36,16 +41,24 @@ public class StartMenuTransition : MonoBehaviour
         TransitionManager.IsPlaying = true;
 
         _deactivated.SetActive(false);
+        _nextMenu.SetActive(true);
 
         _titleRect.DOAnchorPos(_targetRect.anchoredPosition, _Duration).SetEase(Ease.OutSine);
         _titleRect.DOSizeDelta(_targetRect.sizeDelta, _Duration).SetEase(Ease.OutSine);
 
-        _nextMenuCanvasGroup.alpha = 0f;
-        _nextMenuCanvasGroup.DOFade(1, _Duration).OnComplete(OnToTweenComplete);
+        _buttonsCanvasGroup.alpha = 0f;
+        _buttonsCanvasGroup.DOFade(1, _Duration + 0.5f).OnComplete(OnToTweenComplete);
 
         _fishes.DOColor(_endColor, _Duration);
 
-        _nextMenu.SetActive(true);
+
+        _boueeShadows.alpha = 0f;
+
+
+        var seq = DOTween.Sequence();
+        seq.Append(_tweenersBouee2.DoTweenTo(_Duration));
+        seq.Append(_boueeShadows.DOFade(1, 0.5f));
+        _tweenersBouee1.DoTweenTo(_Duration + 0.3f);
     }
 
     private void OnToTweenComplete()
@@ -64,9 +77,15 @@ public class StartMenuTransition : MonoBehaviour
         _titleRect.DOAnchorPos(_startRect.anchoredPosition, _Duration).SetEase(Ease.OutSine);
         _titleRect.DOSizeDelta(_startRect.sizeDelta, _Duration).SetEase(Ease.OutSine);
 
-        _nextMenuCanvasGroup.DOFade(0f, _Duration).OnComplete(OnFromTweenComplete);
+        _buttonsCanvasGroup.DOFade(0f, _Duration).OnComplete(OnFromTweenComplete);
 
         _fishes.DOColor(_startColor, _Duration);
+
+        _boueeShadows.alpha = 1f;
+        _boueeShadows.DOFade(0, 0.2f);
+
+        _tweenersBouee1.DoTweenFrom(_Duration/2);
+        _tweenersBouee2.DoTweenFrom(_Duration/2);
     }
 
     private void OnFromTweenComplete()
