44bf438e60cdfe0fb92923fc363eb5f8d05bbd40
Nathan THEOPHILE <ntheophilelb@gmail.com>
2026-09-11 01:41:46 +0200

    fix(core): c pas bien hein

--- Assets/Scripts/Player/Trails/DashTrail.cs
+++ Assets/Scripts/Player/Trails/DashTrail.cs
@@ -81,17 +81,16 @@ public class DashTrail : MonoBehaviour
         lRenderer.color = _VisualRenderer.color;
         lRenderer.flipX = _VisualRenderer.flipX;
         lRenderer.flipY = _VisualRenderer.flipY;
-        lRenderer.drawMode = _VisualRenderer.drawMode;
-        lRenderer.size = _VisualRenderer.size;
-        lRenderer.maskInteraction = _VisualRenderer.maskInteraction;
-        lRenderer.sortingLayerID = _VisualRenderer.sortingLayerID;
-        lRenderer.sortingOrder = _VisualRenderer.sortingOrder;
-
+        lRenderer.drawMode = SpriteDrawMode.Simple;
+        lRenderer.transform.rotation = transform.rotation * Quaternion.Euler(0f, 0f, 90f);
         Vector2 lSpriteSize = _VisualRenderer.sprite.bounds.size;
         lRenderer.transform.localScale = new Vector3(
             _SquareSize / lSpriteSize.x,
             _SquareSize / lSpriteSize.y,
             1f);
+        lRenderer.maskInteraction = _VisualRenderer.maskInteraction;
+        lRenderer.sortingLayerID = _VisualRenderer.sortingLayerID;
+        lRenderer.sortingOrder = _VisualRenderer.sortingOrder;
     }
 
     public void RemoveSquare(Collider2D pCollider)
