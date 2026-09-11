3b67e0cf4a87a3890f7d353520e72c7ef3b04a09
jonah_bortot <j.bortot@student.isartdigital.com>
2026-09-11 04:37:36 +0200

    dash channeling effect

---
+++ Assets/Scripts/Juice/CircleParticles.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class CircleParticleSystem : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+
+    [Header("Particle Settings")]
+    [SerializeField] private int particleCount = 20;
+    [SerializeField] private float radius = 3f;
+    [SerializeField] private float _startSpeed = 6f;
+
+    [Header("Rotation")]
+    [SerializeField] private float rotationOffset = 0f;
+
+    private ParticleSystem.Particle[] particles;
+
+    private void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        CreateParticles();
+    }
+
+    public void CreateParticles()
+    {
+        _particleSystem.Emit(particleCount);
+
+        particles = new ParticleSystem.Particle[particleCount];
+
+        int count = _particleSystem.GetParticles(particles);
+
+        for (int i = 0; i < count; i++)
+        {
+            float angle = Random.value * 360f;
+
+            float angleRadians = angle * Mathf.Deg2Rad;
+
+            Vector2 position = new Vector2(
+                Mathf.Cos(angleRadians),
+                Mathf.Sin(angleRadians)
+            ) * radius;
+
+            particles[i].position = position;
+
+            Vector2 directionToCenter = -position;
+
+            float rotation = angle;
+            print(rotation);
+            particles[i].rotation = -rotation + rotationOffset;
+            particles[i].velocity = position * _startSpeed;
+        }
+
+        _particleSystem.SetParticles(particles, count);
+    }
+}
